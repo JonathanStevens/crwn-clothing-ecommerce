285c7d33d8ccd5ee637b2f4f1614c7d93527a096
Jonathan Stevens <jonathan.stevens@moovly.com>
2022-06-28 15:34:21 +0200

    Add sign up component, form input component and button component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,6 +3,7 @@ import {
 	getAuth,
 	signInWithPopup,
 	GoogleAuthProvider,
+	createUserWithEmailAndPassword,
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
@@ -31,7 +32,10 @@ export const signInWithGooglePopup = () =>
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async userAuth => {
+export const createUserDocumentFromAuth = async (
+	userAuth,
+	additionalInformation = {}
+) => {
 	const userDocRef = await doc(db, 'users', userAuth.uid);
 	const userSnapshot = await getDoc(userDocRef);
 
@@ -44,6 +48,7 @@ export const createUserDocumentFromAuth = async userAuth => {
 				displayName,
 				email,
 				createdAt,
+				...additionalInformation,
 			});
 		} catch (error) {
 			console.error('error creating the user', error.message);
@@ -51,4 +56,12 @@ export const createUserDocumentFromAuth = async userAuth => {
 	}
 
 	return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+	if (!email || !password) return;
+
+	const authUser = await createUserWithEmailAndPassword(auth, email, password);
+	// await createUserDocumentFromAuth(authUser);
+	return authUser;
 };
