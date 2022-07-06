a3a60977d54c234a9819f2a7764045493d78c90e
Jonathan Stevens <jonathan.stevens@moovly.com>
2022-07-06 14:08:20 +0200

    Add checkout route and extend cart context to decrease and remove cart items

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -16,11 +16,36 @@ const addCartItem = (cartItems, productToAdd) => {
 	return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+	// find the cart item to remove
+	const existingCartItem = cartItems.find(
+		(cartItem) => cartItem.id === cartItemToRemove.id
+	);
+
+	// if the quantity is equal to 1, remove the item from the cart entirely
+	if (existingCartItem.quantity === 1) {
+		return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+	}
+
+	// return cartItems with matching cart item wth reduced quantity
+	return cartItems.map((cartItem) =>
+		cartItem.id === cartItemToRemove.id
+			? { ...cartItem, quantity: cartItem.quantity - 1 }
+			: cartItem
+	);
+};
+
+const clearCartItem = (cartItems, cartItemToClear) => {
+	return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+};
+
 export const CartContext = createContext({
 	isCartOpen: false,
 	setIsCartOpen: () => {},
 	cartItems: [],
 	addItemToCart: () => {},
+	removeItemFromCart: () => {},
+	clearItemFromCart: () => {},
 	cartCount: 0,
 });
 
@@ -41,10 +66,20 @@ export const CartProvider = ({ children }) => {
 		setCartItems(addCartItem(cartItems, product));
 	};
 
+	const removeItemFromCart = (cartItem) => {
+		setCartItems(removeCartItem(cartItems, cartItem));
+	};
+
+	const clearItemFromCart = (cartItem) => {
+		setCartItems(clearCartItem(cartItems, cartItem));
+	};
+
 	const value = {
 		isCartOpen,
 		setIsCartOpen,
 		addItemToCart,
+		removeItemFromCart,
+		clearItemFromCart,
 		cartItems,
 		cartCount,
 	};
