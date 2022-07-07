f55f2c4c6b4706c16c0193229c01b7e1470c11de
Jonathan Stevens <jonathan.stevens@moovly.com>
2022-07-07 11:52:41 +0200

    Clean up one-off code for loading the categories and products data into Firestore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,19 +1,11 @@
 import { createContext, useState, useEffect } from 'react';
 
-import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
-
-import SHOP_DATA from '../data/shop-data.js';
-
 export const ProductsContext = createContext({
 	products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
 	const [products] = useState([]);
-
-	useEffect(() => {
-		addCollectionAndDocuments('categories', SHOP_DATA, 'title');
-	}, []);
 
 	const value = { products };
 	return (
